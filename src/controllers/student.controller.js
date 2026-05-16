922367e30d0e2c63ff1149b809aab2cba0be943f
Samarth305 <Samarth3005@gmail.com>
2026-05-16 10:47:54 +0530

    feat: add resume upload functionality with Cloudinary integration

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -1,9 +1,12 @@
 const prisma = require('../lib/prisma');
+const cloudinary = require ("../../config/cloudinary");
+const streamifier = require("streamifier");
+
 
 //update the profile
 exports.updateProfile = async (req,res) => {
     try {
-        const studentId = req.user.studentId;
+        const studentId = req.user.userId;
         const{ name , cgpa , dept , institute , resumeUrl } = req.body;
         const student = await prisma.student.update({
             where:{
@@ -41,7 +44,7 @@ exports.updateProfile = async (req,res) => {
 //view all applications
 exports.getMyApplications = async (req,res) => {
     try {
-        const studentId = req.user.studentId;
+        const studentId = req.user.userId;
 
         const applications = await prisma.application.findMany({
             where:{
@@ -77,7 +80,7 @@ exports.getMyApplications = async (req,res) => {
 //dashboard
 exports.getStudentDashboard = async (req,res) => {
     try {
-        const studentId = req.user.studentId;
+        const studentId = req.user.userId;
 
         const totalApplications = await prisma.application.findMany({
             where:{
@@ -108,3 +111,52 @@ exports.getStudentDashboard = async (req,res) => {
         error : err.message;
     }
 }
+
+//upload the resume
+exports.uploadResume = async (req,res) => {
+    try {
+        const studentid = req.user.userId;
+        if(!req.file){
+            return res.status(400).json({
+                error:"No File Uploaded"
+            });
+        }
+        const streamUpload = () => {
+            return new Promise ((resolve,reject) => {
+                const stream = cloudinary.uploader.upload_stream(
+                    {
+                        folder:"resumes",
+                        resource_type:"auto"
+                    },
+                    (error,result)=>{
+                        if(result) resolve(result);
+                        else reject(error);
+                    }
+                );
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+
+        const result = await streamUpload();
+
+        const student = await prisma.student.update({
+            where:{
+                studentId:studentid
+            },
+
+            data:{
+                resumeUrl:result.secure_url
+            }
+        });
+
+        res.json({
+            message:"Resume Uploadede Successfully",
+            resumeUrl:result.secure_url,
+            student
+        })
+    } catch (err) {
+        return res.status(500).json({
+            error: err.message
+        });
+    }
+};
--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const authMiddleware = require('../middleware/auth.middleware');
 const roleMiddleware = require('../middleware/role.middleware');
+const upload = require('../middleware/upload.middleware');
 
-const {updateProfile , getMyApplications , getStudentDashboard} = require('../controllers/student.controller');
+const {updateProfile , getMyApplications , getStudentDashboard, uploadResume} = require('../controllers/student.controller');
 
 router.patch('/profile',authMiddleware,roleMiddleware('student'),updateProfile);
 router.get('/applications',authMiddleware,roleMiddleware('student'),getMyApplications);
 router.get('/dashboard',authMiddleware,roleMiddleware('student'),getStudentDashboard);
+router.post("/upload-resume",authMiddleware,roleMiddleware("student"),upload.single("resume"),uploadResume)
 
 module.exports = router ;
